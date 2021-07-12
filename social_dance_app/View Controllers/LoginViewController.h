d1c42a4afb89fc92412415455a58071c5d1b6145
marin-hyatt <marin.hyatt@gmail.com>
2021-07-12 12:50:09 -0400

    Initial setup

---
+++ social_dance_app/View Controllers/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  social_dance_app
+//
+//  Created by Marin Hyatt on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
